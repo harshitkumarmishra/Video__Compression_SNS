74fe72af756962d02000bb16cf0cc0b34c2312a6
harshitkumarmishra <mishra.7@iitj.ac.in>
2019-11-11 20:58:29 +0530

    Updated video.py

--- harshit_changes.py
+++ harshit_changes.py
@@ -4,7 +4,10 @@ import cv2
 import os
 import numpy as np
 import glob
+import timeit
+import shutil
 
+start = timeit.default_timer()
 os.mkdir('/home/harshit/fft/image/')
 os.mkdir('/home/harshit/fft/image1/')
 def FrameCapture(path):
@@ -19,7 +22,7 @@ def FrameCapture(path):
 
          
 count = int(0) 
-FrameCapture("/home/harshit/fft/naren.mp4") 
+FrameCapture("/home/harshit/fft/video.mp4") 
 print("NUMBER OF FRAMES: ",count)
 
 
@@ -39,7 +42,9 @@ for i in range(count-1):
     #count=2
     m=np.max(np.abs(new[:]))
     arr=np.array([0.001])
-    for thresh in 0.1*arr*m:
+#DONT CHNAGE 0.001
+    for thresh in 0.2*arr*m:
+#CHANGE HERE 0.2
         ind=abs(new)>thresh
         newfilt=np.multiply(new,ind)
         nfilt=np.fft.ifft2(newfilt)
@@ -56,10 +61,14 @@ for i in range(0,cnt-1) :
     height,width,layers=img.shape[:3]
     size=(width,height)
     img_array.append(img)
-    out=cv2.VideoWriter('project.mp4',cv2.VideoWriter_fourcc(*'XVID'),20,size)
+    out=cv2.VideoWriter('/home/harshit/fft/project.mp4',cv2.VideoWriter_fourcc(*'XVID'),20,size)
 
 for i in range(int(len(img_array))):
     out.write(img_array[i])
 out.release()
-os.rmdir('/home/harshit/fft/image/')
-os.rmdir('/home/harshit/fft/image1/')
+shutil.rmtree('/home/harshit/fft/image/')
+shutil.rmtree('/home/harshit/fft/image1/')
+stop = timeit.default_timer()
+print('Time: ', stop - start)  
+b = (os.path.getsize("/home/harshit/fft/video.mp4")-os.path.getsize("/home/harshit/fft/project.mp4"))*100/os.path.getsize("/home/harshit/fft/video.mp4")
+print("Compression Ratio = ",b) # ===uncompressed size /compressed size
